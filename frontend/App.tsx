ef4207d6c6fe237079a106b8e2cca821200efa86
Cathrine Bartnes <cathrine2410@gmail.com>
2020-11-10 14:57:28 +0100

    Forgot to add modified files
    Because of new imports due to new examples folder

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './examples/hooks/useCachedResources';
+import useColorScheme from './examples/hooks/useColorScheme';
+import Navigation from './examples/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
